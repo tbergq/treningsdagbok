a056da9b70b33d2dc7c03daa52d3a7eb33dbe556
Trond Bergquist <trond_bergquist@hotmail.com>
2014-11-21 23:25:03 -0500

    wsgi change

--- treningsdagbok/wsgi.py
+++ treningsdagbok/wsgi.py
@@ -1,26 +1,11 @@
 # -*- coding: utf-8 -*-
 """
 WSGI config for treningsdagbok project.
-
 It exposes the WSGI callable as a module-level variable named ``application``.
-
 For more information on this file, see
 https://docs.djangoproject.com/en/dev/howto/deployment/wsgi/
 """
-
 import os
-import sys
-path = 'home/tbergq/treningsdagbok/'
-if path not in sys.path:
-    sys.path.append(path)
-os.chdir(path)
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "treningsdagbok.settings")
-
-import django
-django.setup()
-
-#from django.core.wsgi import get_wsgi_application
-#application = get_wsgi_application()
-
-import django.core.handlers.wsgi
-application = django.core.handlers.wsgi.WSGIHandler()
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
