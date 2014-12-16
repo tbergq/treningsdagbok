1c4afcb1476ece7955b67f214e0c44de92369a74
Trond Bergquist <trond_bergquist@hotmail.com>
2014-12-15 20:32:54 -0500

    changed workoutservice

--- workout/WorkoutServie.py
+++ workout/WorkoutServie.py
@@ -16,11 +16,6 @@ class ExerciseRegisterService:
         return 1
     
     
-    def get_name(self, reg_id):
-        day_register_object = DayRegister.objects.get(pk=reg_id)
-        day_program_object = program_models.DayProgram.objects.get(pk=day_register_object.day_program_id)
-        week_object = program_models.Week.objects.get(pk=day_program_object.week_id)
-        program_object = program_models.Program.objects.get(pk=week_object.program_id)
-        return "%s - %s/%s" % (program_object.name, week_object.name, day_program_object.name) 
+    
     
     
